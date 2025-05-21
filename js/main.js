ddd818f82e19be424d28ee2c8565d7f68e59c4ca
m <157816264+mmoohk@users.noreply.github.com>
2025-05-21 05:25:45 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('كل سنة وإنتِ حياتي').split('')
+    const titles = ('🤍 لبى شدوقك  ').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
